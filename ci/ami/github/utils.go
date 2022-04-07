42521fe9c85a14edfd33617a9fa875d273c174d5
Abhinav Sinha <abhinav@nirmata.com>
2022-04-08 04:11:43 +0530

    Updated main.go
    Signed-off-by: Abhinav Sinha <abhinav@nirmata.com>

--- ci/ami/github/utils.go
+++ ci/ami/github/utils.go
@@ -3,6 +3,7 @@ package github
 import (
 	"context"
 	"os"
+	"strings"
 
 	"github.com/google/go-github/v42/github"
 	"golang.org/x/oauth2"
@@ -21,4 +22,27 @@ func GetGithubClientCtx(token string) (*github.Client, context.Context) {
 func ListRepos(client *github.Client, ctx context.Context) ([]*github.Repository, error) {
 	repos, _, err := client.Repositories.List(ctx, os.Getenv("GITHUB_REPOSITORY_OWNER"), nil)
 	return repos, err
+}
+
+func CreateIssue(client *github.Client, ctx context.Context) {
+	MyIssue := struct {
+		Title    string
+		Body     string
+		Labels   []string
+		Assignee string
+	}{
+		Title:    "Test issue",
+		Body:     "Test issue body",
+		Labels:   []string{"test-issue-label"},
+		Assignee: "zeborg",
+	}
+	testIssue := &github.IssueRequest{
+		Title:    &MyIssue.Title,
+		Body:     &MyIssue.Body,
+		Labels:   &MyIssue.Labels,
+		Assignee: &MyIssue.Assignee,
+	}
+
+	repoSplit := strings.Split(os.Getenv("GITHUB_REPOSITORY"), "/")
+	client.Issues.Create(ctx, repoSplit[0], repoSplit[1], testIssue)
 }
