b7f3f88599d1d43cc75fbdab9974fd07bc091368
Abhinav Sinha <abhinav@nirmata.com>
2022-04-09 19:11:08 +0530

    Updated main.go
    Signed-off-by: Abhinav Sinha <abhinav@nirmata.com>

--- ci/ami/main.go
+++ ci/ami/main.go
@@ -128,7 +128,6 @@ func main() {
 	checkError(err)
 	if !cmp.Equal(currentAMIBuildConfig, latestAMIBuildConfig) {
 		github.Action(latestAMIBuildConfigFileBytes, AMIBuildConfigFilename)
-
 		log.Printf("Info: Updated \"%s\" with K8s versions from \"%s\" to \"%s\"", AMIBuildConfigFilename, currentAMIBuildConfig.K8sReleases, latestAMIBuildConfig.K8sReleases)
 	} else {
 		log.Printf("Info: \"%s\" is up-to-date.", AMIBuildConfigFilename)
