358feeb4e23fcc417e4773c86c819405886c1834
Abhinav Sinha <abhinav@nirmata.com>
2022-05-03 21:17:44 +0530

    Updated `image-builder.sh`
    Signed-off-by: Abhinav Sinha <abhinav@nirmata.com>

--- ci/ami/image-builder.sh
+++ ci/ami/image-builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/kubernetes-sigs/image-builder.git
-sed 's/capa-ami-/test-capa-ami-/' ./image-builder/images/capi/packer/ami/packer.json
+sed -i 's/capa-ami-/test-capa-ami-/' ./image-builder/images/capi/packer/ami/packer.json
 cd image-builder/images/capi
 make deps-ami
 make build-ami-ubuntu-2004
