0a512a7e0ed79b2a9eab69b4fb206da6d00af0b8
Abhinav Sinha <abhinav@nirmata.com>
2022-05-04 12:25:45 +0530

    Updated files
    Signed-off-by: Abhinav Sinha <abhinav@nirmata.com>

--- ci/ami/image-builder-deps.sh
+++ ci/ami/image-builder-deps.sh
@@ -3,4 +3,3 @@ git clone https://github.com/kubernetes-sigs/image-builder.git
 sed -i 's/capa-ami-/test-capa-ami-/' ./image-builder/images/capi/packer/ami/packer.json
 cd image-builder/images/capi
 make deps-ami
-make build-ami-ubuntu-2004
--- ci/ami/main.go
+++ ci/ami/main.go
@@ -37,6 +37,15 @@ type AMIBuildConfig struct {
 	K8sReleases map[string]string `json:"k8s_releases"`
 }
 
+type AMIBuildConfigDefaults struct {
+	Amazon2    map[string]string `json:"amazon-2"`
+	Centos7    map[string]string `json:"centos-7"`
+	Flatcar    map[string]string `json:"flatcar"`
+	Ubuntu1804 map[string]string `json:"ubuntu-1804"`
+	Ubuntu2004 map[string]string `json:"ubuntu-2004"`
+	Default    map[string]string `json:"default"`
+}
+
 type ReleaseVersion struct {
 	Major int
 	Minor int
@@ -83,7 +92,7 @@ func main() {
 func gh() {
 	log.Println("gh()")
 	var m2, m3 string
-	url := "https://raw.githubusercontent.com/zeborg/capa-action-test/main/stable.txt"
+	url := "https://storage.googleapis.com/kubernetes-release/release/stable.txt"
 	k8sReleaseResponse, err := http.Get(url)
 	checkError(err)
 
@@ -98,7 +107,7 @@ func gh() {
 		m3 = strconv.Itoa(min1Release.Major) + "." + strconv.Itoa(min1Release.Minor-2)
 	}
 
-	url = fmt.Sprintf("https://raw.githubusercontent.com/zeborg/capa-action-test/main/stable-%s.txt", m2)
+	url = fmt.Sprintf("https://storage.googleapis.com/kubernetes-release/release/stable-%s.txt", m2)
 	k8sReleaseResponse, err = http.Get(url)
 	checkError(err)
 
@@ -108,7 +117,7 @@ func gh() {
 	min2Release := BuildReleaseVersion(string(min2))
 	log.Print("Info: min2Release: Major ", min2Release.Major, ", Minor ", min2Release.Minor, ", Patch ", min2Release.Patch)
 
-	url = fmt.Sprintf("https://raw.githubusercontent.com/zeborg/capa-action-test/main/stable-%s.txt", m3)
+	url = fmt.Sprintf("https://storage.googleapis.com/kubernetes-release/release/stable-%s.txt", m3)
 	k8sReleaseResponse, err = http.Get(url)
 	checkError(err)
 
@@ -156,27 +165,115 @@ func gh() {
 
 func presubmit() {
 	AMIBuildConfigFilename := os.Getenv("AMI_BUILD_CONFIG_FILENAME")
+	AMIBuildConfigDefaultsFilename := os.Getenv("AMI_BUILD_CONFIG_DEFAULTS")
+
 	dat, err := os.ReadFile(AMIBuildConfigFilename)
 	checkError(err)
-
 	currentAMIBuildConfig := new(AMIBuildConfig)
 	err = json.Unmarshal(dat, currentAMIBuildConfig)
 	checkError(err)
 
+	dat, err = os.ReadFile(AMIBuildConfigDefaultsFilename)
+	checkError(err)
+	defaultAMIBuildConfig := new(AMIBuildConfigDefaults)
+	err = json.Unmarshal(dat, defaultAMIBuildConfig)
+	checkError(err)
+
 	for _, v := range currentAMIBuildConfig.K8sReleases {
-		err, out, _ := Shell(fmt.Sprintf("./clusterawsadm ami list --kubernetes-version %s", strings.ReplaceAll(v, "v", "")))
+		err, out, _ := Shell(fmt.Sprintf("./clusterawsadm ami list --kubernetes-version %s", strings.TrimPrefix(v, "v")))
 		checkError(err)
 
 		if out == "" {
 			log.Printf("Info: Building AMI for Kubernetes %s.", v)
-			err, out, errout := Shell("sh ./image-builder.sh")
-			checkError(err)
+			kubernetes_semver := v
+			kubernetes_rpm_version := strings.TrimPrefix(v, "v") + "-0"
+			kubernetes_deb_version := strings.TrimPrefix(v, "v") + "-00"
+			kubernetes_series := strings.Split(v, ".")[0] + strings.Split(v, ".")[1]
 
+			flagsK8s := fmt.Sprintf("-var=kubernetes_series=%s -var=kubernetes_semver=%s -var=kubernetes_rpm_version=%s -var=kubernetes_deb_version=%s ", kubernetes_series, kubernetes_semver, kubernetes_rpm_version, kubernetes_deb_version)
+			for k, v := range defaultAMIBuildConfig.Default {
+				flagsK8s += fmt.Sprintf("-var=%s=%s ", k, v)
+			}
+
+			err, out, errout := Shell("sh ./image-builder-deps.sh")
+			checkError(err)
 			if errout != "" {
 				log.Fatalf("Error: %s", errout)
 			} else {
-				log.Println("Info: image-builder log")
+				log.Println("Info: Setting up image-builder")
 				log.Println(out)
+			}
+
+			for _, os := range []string{"amazon-2", "centos-7", "flatcar-stable", "ubuntu-18.04", "ubuntu-20.04"} {
+				switch os {
+				case "amazon-2":
+					flags := ""
+					for k, v := range defaultAMIBuildConfig.Amazon2 {
+						flags += fmt.Sprintf("-var=%s=%s ", k, v)
+					}
+					log.Println(fmt.Sprintf("Info: Building AMI for OS %s", os))
+					err, out, errout := Shell(fmt.Sprintf("PACKER_FLAGS=\"%s\" make build-ami-%s", flags, os))
+					checkError(err)
+					if errout != "" {
+						log.Fatalf("Error: %s", errout)
+					} else {
+						log.Println(out)
+					}
+				case "centos-7":
+					flags := ""
+					for k, v := range defaultAMIBuildConfig.Centos7 {
+						flags += fmt.Sprintf("-var=%s=%s ", k, v)
+					}
+					log.Println(fmt.Sprintf("Info: Building AMI for OS %s", os))
+					err, out, errout := Shell(fmt.Sprintf("PACKER_FLAGS=\"%s\" make build-ami-%s", flags, os))
+					checkError(err)
+					if errout != "" {
+						log.Fatalf("Error: %s", errout)
+					} else {
+						log.Println(out)
+					}
+				case "flatcar":
+					flags := ""
+					for k, v := range defaultAMIBuildConfig.Flatcar {
+						flags += fmt.Sprintf("-var=%s=%s ", k, v)
+					}
+					log.Println(fmt.Sprintf("Info: Building AMI for OS %s", os))
+					err, out, errout := Shell(fmt.Sprintf("PACKER_FLAGS=\"%s\" make build-ami-%s", flags, os))
+					checkError(err)
+					if errout != "" {
+						log.Fatalf("Error: %s", errout)
+					} else {
+						log.Println(out)
+					}
+				case "ubuntu-1804":
+					flags := ""
+					for k, v := range defaultAMIBuildConfig.Ubuntu1804 {
+						flags += fmt.Sprintf("-var=%s=%s ", k, v)
+					}
+					log.Println(fmt.Sprintf("Info: Building AMI for OS %s", os))
+					err, out, errout := Shell(fmt.Sprintf("PACKER_FLAGS=\"%s\" make build-ami-%s", flags, os))
+					checkError(err)
+					if errout != "" {
+						log.Fatalf("Error: %s", errout)
+					} else {
+						log.Println(out)
+					}
+				case "ubuntu-2004":
+					flags := ""
+					for k, v := range defaultAMIBuildConfig.Ubuntu2004 {
+						flags += fmt.Sprintf("-var=%s=%s ", k, v)
+					}
+					log.Println(fmt.Sprintf("Info: Building AMI for OS %s", os))
+					err, out, errout := Shell(fmt.Sprintf("PACKER_FLAGS=\"%s\" make build-ami-%s", flags, os))
+					checkError(err)
+					if errout != "" {
+						log.Fatalf("Error: %s", errout)
+					} else {
+						log.Println(out)
+					}
+				default:
+					log.Println(fmt.Sprintf("Warning: Invalid OS %s. Skipping image building.", os))
+				}
 			}
 		} else {
 			log.Printf("Info: AMI for Kubernetes %s already exists. Skipping image building.", v)
